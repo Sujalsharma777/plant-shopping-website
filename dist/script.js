07cd74fa9229235c4a7c78719c2fc5df4dbd2b7d
Sujal_sharma777 <115543306+Sujalsharma777@users.noreply.github.com>
2023-12-14 16:23:07 +0530

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,13 @@
+// let logoname = document.querySelector("#icon")
+// function toggleMenu() {
+//     var menuBox = document.getElementById('menu-box');    
+//     if(menuBox.style.display == "block") { // if is menuBox displayed, hide it
+//       menuBox.style.display = "none";
+//       logoname.className ="fa-solid fa-plus" 
+//     }
+//     else { // if is menuBox hidden, display it
+//       menuBox.style.display = "block";
+//       logoname.className ="fa-solid fa-close " 
+
+//     }
+//   }  
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    // colors:{
+    //   'lightgreen':'#cedebd',
+    //   'dark-green': '#435334',
+    //   'default': '#616161',
+    //   'black':'#000000',
+      
+    // },
+    fontFamily:{
+      'poppins':['Poppins', 'sans-serif'],
+      'Mandali':['Mandali', 'sans-serif'],
+      'Halant':['Halant', 'serif'],
+      'Martel':['Martel', 'serif']
+    },
+    
+  },
+  plugins: [],
+}
+
