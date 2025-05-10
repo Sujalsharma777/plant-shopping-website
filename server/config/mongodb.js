982ae545f88ae152de2a458c4a97e2c808782ee8
Sujal_sharma777 <115543306+Sujalsharma777@users.noreply.github.com>
2025-05-10 10:10:32 +0530

    Delete server directory

--- server/config/mongodb.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  mongoose.connection.on("connect", () => console.log("database connect"));
-  await mongoose.connect(`{process.env.MONGODB_URL}/ecommerce-project`);
-};
-export default connectDB;
