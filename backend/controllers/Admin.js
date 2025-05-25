b40fc0ea06ff860f560ab6e16a9e3bfdc5c9e913
Sujal_sharma777 <115543306+Sujalsharma777@users.noreply.github.com>
2025-05-25 23:38:54 +0530

    Update Admin.js

--- backend/controllers/Admin.js
+++ backend/controllers/Admin.js
@@ -24,9 +24,7 @@ const loginAdminUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Lax",
-       maxAge: 24 * 60 * 60 * 1000
+      
     });
 
     res
